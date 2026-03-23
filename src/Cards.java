ec2e4e6efee4266015049a4341fe117e8f6f13be
Ege <ege.usug@gmail.com>
2026-03-23 17:40:29 +0300

    structure done i guess

---
+++ src/Cards.java
@@ -0,0 +1,18 @@
+public class Cards {
+
+    private String symbol;
+    private int number;
+
+    public String getSymbol(){
+        return symbol;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public void setSymbol(String symbol){
+        this.symbol = symbol;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+}
---
+++ src/Logic.java
@@ -0,0 +1,3 @@
+public class Logic {
+    //kartlari olusturup bunlari bir arraye ata ardindan alinan actiona gore desteden rastgele kart ver dongu icersinde
+}
